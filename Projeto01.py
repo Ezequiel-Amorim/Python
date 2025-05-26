9efd8f8cd94d2863b4cd924190615146dce0a97f
Ezequiel-Amorim <contaezequielamorim@gmail.com>
2025-05-26 14:37:23 -0300

    Update Projeto01.py

--- Projeto01.py
+++ Projeto01.py
@@ -1,6 +1,6 @@
 from deep_translator import GoogleTranslator #biblioteca que usa o tradutor do google
 
-#traducao = GoogleTranslator(source='pt', target='en').translate('Oi mano Ruan vai querer uma café daqui a pouco ?')
+#traducao = GoogleTranslator(source='pt', target='en').translate('Oi pessoal de boas ?')
 #print(traducao)
 
 #Anotações importantes a importação do GoogleTranslator(usa o tradutor do google e faz a tradução) para fazer a tradução tenho que colocar qual idioma que está no 'source'que seria a linguagem de entrada que sera escrita para a linguagem que deseja traduzir |'target' é a linguagem que desejo transforma meu texto para o idioma desejado
